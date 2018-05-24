3f61a9640d948fd39f4fdbcae6c35a7808d9db9f
Duca Vitalie-Alexandru <vixan.one@gmail.com>
2018-05-24 12:10:12 +0300

    🗃 Update database to cascade delete sections and criteria
    When deleting a Form the Sections and Criteria are deleted on cascade automatically.

--- EvaluationFormsManager/EvaluationFormsManager.Persistence.EF/Migrations/20180524090412_AddFormCascadeDelete.Designer.cs
+++ EvaluationFormsManager/EvaluationFormsManager.Persistence.EF/Migrations/20180524090412_AddFormCascadeDelete.Designer.cs
@@ -4,14 +4,16 @@ using EvaluationFormsManager.Persistence.EF;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace EvaluationFormsManager.Persistence.EF.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20180524090412_AddFormCascadeDelete")]
+    partial class AddFormCascadeDelete
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
